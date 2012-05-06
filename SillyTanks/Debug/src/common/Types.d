cf31c889f24a60b8e297f7df9a29f4b2c72a30fe
Benjamin Ellenberger <benjamin.ellenberger@zoho.com>
2012-05-06 17:06:01 +0200

    Cleaned up the project as the debug folder and a folder called elements was still under version control. The elements folder just contains stuff to inspire the AI and the particle engine. So it does not have to be under version control.

--- SillyTanks/Debug/src/common/Types.d
+++
@@ -1,6 +0,0 @@
-src/common/Types.d src/common/Types.o: ../src/common/Types.cpp \
- ../src/common/Types.hpp ../src/common/Definitions.hpp
-
-../src/common/Types.hpp:
-
-../src/common/Definitions.hpp:
