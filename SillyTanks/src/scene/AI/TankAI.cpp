feae10b92bbd614ba08812937c76ae36abc1a6f0
Benjamin Ellenberger <benjamin.ellenberger@zoho.com>
2012-05-29 14:19:53 +0200

    .

--- SillyTanks/src/scene/AI/TankAI.cpp
+++ SillyTanks/src/scene/AI/TankAI.cpp
@@ -233,8 +233,7 @@ void TankAI::aim() {
 		for (float i = 1; i < 30; i++) {
 			float angle = Utils::getElevation(_tank->getPosition(), _currentTarget->getPosition(), i, false, 1);
 			if (angle != -1) {
-				std::cout << angle << "\n";
-				_tank->setElevation(rand()%10-5+angle-10+_tank->currentTilt);
+				_tank->setElevation(rand()%10+angle-10+_tank->currentTilt);
 				_tank->setShootingPower(i / 30);
 				break;
 			}
